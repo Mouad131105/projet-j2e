7b8ea6d566c825555252541cba4710ffaa2851d5
Mouad131105 <mouad.aouida@gmail.com>
2023-03-01 00:52:03 +0100

    Create QuestionController and questions.html for displaying and managing questions

---
+++ src/main/java/fr/uge/jee/ugeoverflow/controller/QuestionController.java
@@ -0,0 +1,30 @@
+package fr.uge.jee.ugeoverflow.controller;
+
+import fr.uge.jee.ugeoverflow.entities.Question;
+import fr.uge.jee.ugeoverflow.repository.QuestionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class QuestionController {
+    QuestionRepository questionRepository;
+
+    @GetMapping("/questions")
+    public String questions(Model model,
+                            @RequestParam(name = "page", defaultValue = "0") int page,) {
+        Page<Question> questions = questionRepository.findAll(PageRequest.of(page, 10));
+        model.addAttribute("listQuestions", questions.getContent());
+        model.addAttribute("pages", new int[questions.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        return "questions";
+
+    }
+
+}
