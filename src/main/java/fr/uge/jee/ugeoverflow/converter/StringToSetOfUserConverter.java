715576ceaa55182884b0945a5f9d474a470400a1
natan-burin <burnatan@gmail.com>
2023-03-03 19:09:16 +0100

    Added config class And method converter (not used)

---
+++ src/main/java/fr/uge/jee/ugeoverflow/converter/StringToSetOfUserConverter.java
@@ -0,0 +1,21 @@
+package fr.uge.jee.ugeoverflow.converter;
+
+import fr.uge.jee.ugeoverflow.entities.User;
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.*;
+
+public class StringToSetOfUserConverter implements Converter<String, Set<User>> {
+
+    StringToUserConverter stringToUserConverter = new StringToUserConverter();
+
+    @Override
+    public Set<User> convert(String s) {
+        List<String> data = Arrays.asList(s.split(","));
+        Set<User> res = new HashSet<>();
+        data.forEach(x ->
+                res.add(stringToUserConverter.convert(x))
+        );
+        return res;
+    }
+}
