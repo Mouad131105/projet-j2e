4d8240a14b97bd868612e18d4a318a2d642b54f3
Mouad131105 <mouad.aouida@gmail.com>
2023-03-18 02:39:42 +0100

    Update processLogin method in UserController to display sorted questions
    - Add fetching all users and passing them to the model
    - Replace findAll with getSortedQuestionsByFollowing to fetch sorted questions for the logged-in user
    - Fetch other questions and remove sorted questions from the list
    - Combine sorted questions and other questions to display on the home page
    - Update the page attribute names and return the home-page view instead of home-page-questions

--- src/main/java/fr/uge/jee/ugeoverflow/user/UserController.java
+++ src/main/java/fr/uge/jee/ugeoverflow/user/UserController.java
@@ -6,11 +6,14 @@ import fr.uge.jee.ugeoverflow.note.NoteService;
 import fr.uge.jee.ugeoverflow.publishing.question.Question;
 import fr.uge.jee.ugeoverflow.publishing.question.QuestionService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -125,11 +128,12 @@ public class UserController {
         return "user-registration";
     }
 
-    /*@GetMapping("/login")
+    @GetMapping("/login")
     public String loginForm() {
         return "login-form";
     }
 
+    /*
     @PostMapping("/login")
     public String processLogin(@RequestParam(name = "username") @NotEmpty String username,
                               @RequestParam(name = "password") @NotEmpty String password,
@@ -150,6 +154,40 @@ public class UserController {
         return "login-form";
     }*/
 
+    @PostMapping("/login")
+    public String processLogin(@RequestParam(name = "username") @NotEmpty String username,
+                               @RequestParam(name = "password") @NotEmpty String password,
+                               Model model) {
+
+        List<User> users = this.userService.getAllUsers();
+        if(!users.isEmpty()){
+            model.addAttribute("allUsers",users);
+        }
+
+        User user = this.userService.findUserByUsername(username);
+        if (user != null && user.getPassword().equals(password)) {
+            model.addAttribute("loggedUser", user);
+
+            List<Question> sortedQuestions = questionService.getSortedQuestionsByFollowing(user);
+            List<Question> otherQuestions = (List<Question>) questionService.findAll(0, 5);
+
+            // Supprimez les questions déjà triées de la liste des autres questions
+            otherQuestions.removeAll(sortedQuestions);
+
+            // Ajoutez les autres questions après les questions triées
+            sortedQuestions.addAll(otherQuestions);
+
+            Page<Question> questions = new PageImpl<>(sortedQuestions, PageRequest.of(0, 5), sortedQuestions.size());
+            model.addAttribute("listQuestions", questions.getContent());
+            model.addAttribute("pages", new int[questions.getTotalPages()]);
+            model.addAttribute("currentPage", 0);
+            return "home-page";
+        }
+
+        model.addAttribute("loginError", "Username or password is incorrect");
+
+        return "login-form";
+    }
     @RequestMapping(value = "/homepage/questions", method = {RequestMethod.POST, RequestMethod.GET})
     public String homepageQuestions(Model model){
         User user = this.authenticationService.getLoggedUser();
