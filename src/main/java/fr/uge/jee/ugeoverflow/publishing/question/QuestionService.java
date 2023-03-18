f95ad8091ccf69428a42d4f192ed5001a1de37c3
Mouad131105 <mouad.aouida@gmail.com>
2023-03-18 02:18:48 +0100

    Add findByAuthor in QuestionRepository and add getSortedQuestionsByFollowing in QuestionService
    - Add findByAuthor method in QuestionRepository to find questions by their author
    - Add getSortedQuestionsByFollowing method in QuestionService to fetch and sort questions:
      1. Fetch questions from followed users
      2. Fetch questions from users followed by followed users
      3. Combine and sort the fetched questions

--- src/main/java/fr/uge/jee/ugeoverflow/publishing/question/QuestionService.java
+++ src/main/java/fr/uge/jee/ugeoverflow/publishing/question/QuestionService.java
@@ -1,10 +1,12 @@
 package fr.uge.jee.ugeoverflow.publishing.question;
 
 import fr.uge.jee.ugeoverflow.publishing.Tag;
+import fr.uge.jee.ugeoverflow.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -26,8 +28,26 @@ public class QuestionService {
     public Page<Question> findByTopicContains(String keyword, PageRequest of){
         return questionRepository.findByTopicContains(keyword,of);
     }
-
     public Page<Question> findByTag(String tag, PageRequest of){ return questionRepository.findByTagsContaining(Set.of(Tag.valueOf(tag)),of);}
+
+    public List<Question> getQuestionsByUser(User user) {
+        return questionRepository.findByAuthor(user);
+    }
+    public List<Question> getSortedQuestionsByFollowing(User loggedUser) {
+        List<User> following = new ArrayList<>(loggedUser.getFollowedUsers());
+        System.out.println("Followed user:  " + following.size());
+        List<Question> sortedQuestions = new ArrayList<>();
+
+        for (User user : following) {
+            sortedQuestions.addAll(getQuestionsByUser(user));
+        }
+        for (User user : following) {
+            for (User followedUser : user.getFollowedUsers()) {
+                sortedQuestions.addAll(getQuestionsByUser(followedUser));
+            }
+        }
+        return sortedQuestions;
+    }
     public Question findQuestionById(Long id) {
         return this.questionRepository.findQuestionById(id);
     }
